3ca2debdd57ce9910fc2a4c85a56283434e1f4ae
Daniel Milz <daniel.milz@icloud.com>
2019-07-06 14:09:38 +0200

    Started implementing disparity_map.m with image reading and calib.txt reading

--- disparity_map.m
+++ disparity_map.m
@@ -3,6 +3,17 @@ function [D, R, T] = disparity_map(scene_path)
     % disparity map of the included stereo image pair. Also, the Euclidean
     % motion is returned as Rotation R and Translation T.
     
+    %% Do some typical error checking here
     
+    
+    %% Load images and calibration file from scene folder
+    im0 = imread([scene_path '/im0.png']);
+    im1 = imread([scene_path '/im1.png']);
+    cal = fileread([ scene_path '/calib.txt']);
+
+    %% Create calibration variables
+    evalc(cal);
+    
+    %% 
     
 end
