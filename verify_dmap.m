c35397ee007bfa6452e7811e530dd91c75b6360f
Daniel Milz <daniel.milz@icloud.com>
2019-07-06 14:54:43 +0200

    Added basic range checking for verify_dmap.m

--- verify_dmap.m
+++ verify_dmap.m
@@ -2,5 +2,9 @@ function p = verify_dmap(D, G)
 % This function calculates the PSNR of a given disparity map and the ground
 % truth. The value range of both is normalized to [0,255].
 
+%% Check if D and G are in range [0 255]
+assert(any((G <= 255 & G >= 0) == 0), 'G has to be in range [0 255]');
+assert(any((D <= 255 & D >= 0) == 0), 'D has to be in range [0 255]');
+
 end
 
