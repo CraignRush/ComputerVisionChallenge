b14395cb5835d426b3d2e454ac7a97a019cde9fa
Daniel Milz <daniel.milz@icloud.com>
2019-07-06 14:36:20 +0200

    Added basic testing function from disparity_map.m. Figured out, that from correspondence estimation on there is an error (correspondence is empty...)

---
+++ test/test_disparity_map.m
@@ -0,0 +1,13 @@
+
+
+%% Motorcycle
+[D, R, T] = disparity_map('test/motorcycle')
+
+%% Playground
+[D, R, T] = disparity_map('test/playground')
+
+%% Sword
+[D, R, T] = disparity_map('test/sword')
+
+%% Terrace
+[D, R, T] = disparity_map('test/terrace')
