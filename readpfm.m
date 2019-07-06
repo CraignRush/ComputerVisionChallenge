30d1ea888fec2d6ca2db3a9b29846f3e113e87da
Johann Brenner <johann.brenner@tum.de>
2019-07-06 22:49:53 +0200

    Debugged pfm parser

--- readpfm.m
+++ readpfm.m
@@ -1,13 +1,37 @@
-function D = readpfm(filename_pfm)
+function [D,header] = readpfm(filename_pfm)
 
 fid = fopen(filename_pfm);
 
-fscanf(fid,'%c',[1,3]);
-cols = fscanf(fid,'%f',1);
-rows = fscanf(fid,'%f',1);
-fscanf(fid,'%f',1);
+header{1} = fscanf(fid,'%c',[1 2]);
+header{2} = fscanf(fid,'%f',[1 2]);
+header{3} = fscanf(fid,'%f',1);
 fscanf(fid,'%c',1);
-D = fread(fid,[cols,rows],'single');
+
+if header{3} > 0
+    % scale factor > 0 --> big endian encoded file
+    endianness = 'ieee-be';
+else
+    % scale factor < 0 --> little endian encoded file
+    endianness = 'ieee-le';
+end
+
+if header{1} == 'Pf'
+    % grayscale image
+    D = fread(fid,[header{2}(1) header{2}(2)],'single',0,endianness);
+elseif header{1} == 'PF'
+    % color image
+    tmp = fread(fid,[header{2}(1)*3 header{2}(2)],'single',0,endianness); 
+    D = zeros([header{2} 3]);
+    D(:,:,1) = tmp(1:3:end,:);
+    D(:,:,2) = tmp(2:3:end,:);
+    D(:,:,3) = tmp(3:3:end,:);
+else
+    error('Header could not be read!');
+end
+
 D(D == Inf) = 0;
 D = rot90(D);
 fclose(fid);
+
+
+
