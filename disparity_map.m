854d319593ddb5c3c92b6b4b78941d5abda8ad9a
Daniel Milz <daniel.milz@icloud.com>
2019-07-06 14:14:57 +0200

    Added basic feature ecxtractin to disparity_map.m

--- disparity_map.m
+++ disparity_map.m
@@ -3,6 +3,8 @@ function [D, R, T] = disparity_map(scene_path)
     % disparity map of the included stereo image pair. Also, the Euclidean
     % motion is returned as Rotation R and Translation T.
     
+    addpath(genpath('lib')); 
+    
     %% Do some typical error checking here
     
     
@@ -14,6 +16,7 @@ function [D, R, T] = disparity_map(scene_path)
     %% Create calibration variables
     evalc(cal);
     
-    %% 
-    
+    %% Feature extraction
+    feature0 = harris_detektor( rgb_to_gray(im0) );
+    feature1 = harris_detektor( rgb_to_gray(im1) );
 end
