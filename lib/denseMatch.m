2b032e6bb6b13b71ed2401eb4cdaf3697bb5a305
Daniel Milz <daniel.milz@icloud.com>
2019-07-19 23:08:16 +0200

    Removed unnecessary stuff from denseMap.m

--- lib/denseMatch.m
+++ lib/denseMatch.m
@@ -1,88 +1,31 @@
 % *************************************************************************
-% Title: Function-Compute Correlation between two images using
-% various
-% similarity measures with Left Image as reference.
-% Author: Siddhant Ahuja
-% Created: March 2010
-% Copyright Siddhant Ahuja, 2010
-% Inputs:
-% 1. Left Image (var: rightImage),
-% 2. Right Image (var: leftImage),
-% 3. Correlation Window Size (var: corrWindowSize),
-% 4. Minimum Disparity in X-direction (var: dMin),
-% 5. Maximum Disparity in X-direction (var: dMax),
-% 6. Method used for calculating the correlation scores (var:
-% method)
-% Valid values include: 'SAD', 'LSAD', 'ZSAD', 'SSD', 'LSSD',
-% ZSSD', 'NCC',
-% 'ZNCC'
-% Outputs:
-% 1. Disparity Map (var: dispMap),
-% 2. Time taken (var: timeTaken)
-% Example Usage of Function: [dispMap,
-% timeTaken]=denseMatch('tsukuba_left.tiff', 'tsukuba_right.tiff',
-% 9, 0, 16, 'ZNCC');
+% denseMatch.mn
 % *************************************************************************
-function dispMap = denseMatch(rightImage, leftImage, ...
-    corrWindowSize, dMin, dMax, ...
-    method)
+function dispMap = denseMatch(rightImage, leftImage, corrWindowSize,...
+    dMin, dMax)
 
-% Find the size (columns and rows) of the left image and assign the
-% rows to
-% variable nrLeft, and columns to variable ncLeft
+
 [nrLeft,ncLeft] = size(leftImage);
-% Find the size (columns and rows) of the right image and assign
-% the rows to
-% variable nrRight, and columns to variable ncRight
-[nrRight,ncRight] = size(rightImage);
-% Check to see if both the left and right images have same number
-% of rows
-% and columns
-if(nrLeft==nrRight && ncLeft==ncRight)
-else
-    error(['Both left and right images should have the same number ' ...
-        'of rows and columns']);
-end
+
 % Convert the left and right images from uint8 to double
 leftImage=im2double(leftImage);
 rightImage=im2double(rightImage);
-% Check the size of window to see if it is an odd number.
-if (mod(corrWindowSize,2)==0)
-    error('The window size must be an odd number.');
-end
-% Check whether minimum disparity is less than the maximum
-% disparity.
-if (dMin>dMax)
-    error(['Minimum Disparity must be less than the Maximum ' ...
-        'disparity.']);
-end
+
 % Create an image of size nrLeft and ncLeft, fill it with zeros and
-% assign
-% it to variable dispMap
+% assign it to variable dispMap
 dispMap=zeros(nrLeft, ncLeft);
+
 % Find out how many rows and columns are to the left/right/up/down
-% of the
-% central pixel based on the window size
+% of the central pixel based on the window size
 win=(corrWindowSize-1)/2;
-% The objective of CC, NCC and ZNCC is to maxmize the
-% correlation score, whereas other methods try to minimize
-% it.
-maximize = 0;
-if strcmp(method,'NCC') || strcmp(method,'ZNCC')
-    maximize = 1;
-end
 
 for(i=1+win:1:nrLeft-win)
     % For every row in Left Image
     for(j=1+win:1:ncLeft-win-dMax)
         % For every column in Left Image
-        % Initialize the temporary variable to hold the previous
-        % correlation score
-        if(maximize)
-            prevcorrScore = 0.0;
-        else
-            prevcorrScore = 65532;
-        end
+
+        prevcorrScore = 0.0;
+        
         % Initialize the temporary variable to store the best
         % matched
         % disparity score
@@ -94,67 +37,33 @@ for(i=1+win:1:nrLeft-win)
             regionLeft=leftImage(i-win : i+win, j-win : j+win);
             % Construct a region with window around
             % central/selected pixel in right image
-            regionRight=rightImage(i-win : i+win, j+d-win : j+d+ ...
-                win);
+            regionRight=rightImage(i-win : i+win, j+d-win : j+d+ win);
             % Calculate the local mean in left region
             meanLeft = mean2(regionLeft);
             % Calculate the local mean in right region
             meanRight = mean2(regionRight);
-            % Initialize the variable to store temporarily the
-            % correlation scores
-            tempCorrScore = zeros(size(regionLeft));
+
             % Calculate the correlation score
-            if strcmp(method,'SAD')
-                tempCorrScore = abs(regionLeft - regionRight);
-            elseif strcmp(method,'ZSAD')
-                tempCorrScore = abs(regionLeft - meanLeft - regionRight ...
-                    + meanRight);
-            elseif strcmp(method,'LSAD')
-                tempCorrScore = abs(regionLeft - meanLeft/ ...
-                    meanRight*regionRight);
-            elseif strcmp(method,'SSD')
-                tempCorrScore = (regionLeft - regionRight).^2;
-            elseif strcmp(method,'ZSSD')
-                tempCorrScore = (regionLeft - meanLeft - regionRight ...
-                    + meanRight).^2;
-            elseif strcmp(method,'LSSD')
-                tempCorrScore = (regionLeft - meanLeft/meanRight* ...
-                    regionRight).^2;
-            elseif strcmp(method,'NCC')
-                % Calculate the term in the denominator (var: den)
-                den = sqrt(sum(sum(regionLeft.^2))* ...
-                    sum(sum(regionRight.^2)));
-                tempCorrScore = regionLeft.*regionRight/den;
-            elseif strcmp(method,'ZNCC')
-                % Calculate the term in the denominator (var: den)
-                den = sqrt(sum(sum((regionLeft - meanLeft).^2))* ...
-                    sum(sum((regionRight - meanRight).^2)));
-                tempCorrScore = (regionLeft - meanLeft).*(regionRight ...
-                    - ...
-                    meanRight)/den;
-            end
+            % Calculate the term in the denominator (var: den)
+            den = sqrt(sum(sum((regionLeft - meanLeft).^2))* ...
+                sum(sum((regionRight - meanRight).^2)));
+            tempCorrScore = (regionLeft - meanLeft).*(regionRight ...
+                - meanRight)/den;
+            
             % Compute the final score by summing the values in
             % tempCorrScore,
             % and store it in a temporary variable signifying the
             % distance
             % (var: corrScore)
             corrScore=sum(sum(tempCorrScore));
-            if(maximize)
-                if(corrScore>prevcorrScore)
-                    % If the current disparity value is greater
-                    % than
-                    % previous one, then swap them
-                    prevcorrScore=corrScore;
-                    bestMatchSoFar=d;
-                end
-            else
-                if (prevcorrScore > corrScore)
-                    % If the current disparity value is less than
-                    % previous one, then swap them
-                    prevcorrScore = corrScore;
-                    bestMatchSoFar = d;
-                end
+            if(corrScore>prevcorrScore)
+                % If the current disparity value is greater
+                % than
+                % previous one, then swap them
+                prevcorrScore=corrScore;
+                bestMatchSoFar=d;
             end
+
         end
         % Store the final matched value in variable dispMap
         dispMap(i,j) = bestMatchSoFar;
