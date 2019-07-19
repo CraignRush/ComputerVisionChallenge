c7351e575589056f28dc3c25a64568d36a2d432c
Johann Brenner <johann.brenner@tum.de>
2019-07-19 19:30:24 +0200

    Added dependent libs

---
+++ denseMatch/denseMatch_types.h
@@ -0,0 +1,83 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * denseMatch_types.h
+ *
+ * Code generation for function 'denseMatch'
+ *
+ */
+
+#ifndef DENSEMATCH_TYPES_H
+#define DENSEMATCH_TYPES_H
+
+/* Include files */
+#include "rtwtypes.h"
+
+/* Type Definitions */
+#ifndef struct_emxArray_char_T
+#define struct_emxArray_char_T
+
+struct emxArray_char_T
+{
+  char_T *data;
+  int32_T *size;
+  int32_T allocatedSize;
+  int32_T numDimensions;
+  boolean_T canFreeData;
+};
+
+#endif                                 /*struct_emxArray_char_T*/
+
+#ifndef typedef_emxArray_char_T
+#define typedef_emxArray_char_T
+
+typedef struct emxArray_char_T emxArray_char_T;
+
+#endif                                 /*typedef_emxArray_char_T*/
+
+#ifndef struct_emxArray_real_T
+#define struct_emxArray_real_T
+
+struct emxArray_real_T
+{
+  real_T *data;
+  int32_T *size;
+  int32_T allocatedSize;
+  int32_T numDimensions;
+  boolean_T canFreeData;
+};
+
+#endif                                 /*struct_emxArray_real_T*/
+
+#ifndef typedef_emxArray_real_T
+#define typedef_emxArray_real_T
+
+typedef struct emxArray_real_T emxArray_real_T;
+
+#endif                                 /*typedef_emxArray_real_T*/
+
+#ifndef struct_emxArray_uint8_T
+#define struct_emxArray_uint8_T
+
+struct emxArray_uint8_T
+{
+  uint8_T *data;
+  int32_T *size;
+  int32_T allocatedSize;
+  int32_T numDimensions;
+  boolean_T canFreeData;
+};
+
+#endif                                 /*struct_emxArray_uint8_T*/
+
+#ifndef typedef_emxArray_uint8_T
+#define typedef_emxArray_uint8_T
+
+typedef struct emxArray_uint8_T emxArray_uint8_T;
+
+#endif                                 /*typedef_emxArray_uint8_T*/
+#endif
+
+/* End of code generation (denseMatch_types.h) */
